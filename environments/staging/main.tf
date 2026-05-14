783652fdf9a399fa112ec6a326975412ba967240
mostafals <mostafa.lagnaoui@gmail.com>
2026-05-14 11:42:48 +0100

    add appservice to staging

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -29,3 +29,15 @@ module "storage" {
   environment         = var.environment
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+############# MODULE APPSERVICE ###############
+module "appservice" {
+  source = "../../modules/appservice"
+
+  location            = var.location
+  project_name        = var.project_name
+  environment         = var.environment
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = module.networking.subnet_id
+}
+### merge
