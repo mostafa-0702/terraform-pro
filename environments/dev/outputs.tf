066506b65c62455024c88bd8d584a8cc9be85018
mostafals <mostafa.lagnaoui@gmail.com>
2026-05-14 10:12:49 +0100

    initial setup - modules, dev environment, pipelines

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  value = module.networking.vnet_name
+}
+
+output "subnet_name" {
+  value = module.networking.subnet_name
+}
+
+output "storage_account_name" {
+  value = module.storage.storage_account_name
+}
+
+output "primary_blob_endpoint" {
+  value = module.storage.primary_blob_endpoint
+}
